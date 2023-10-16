d1c760940dc8b2f268a5c3e27155805ec8dea77a
ch2oh-ch2oh <valerasofronov01@gmail.com>
2023-10-16 13:49:14 +0300

    1 fix

--- test_1.py
+++ test_1.py
@@ -1,8 +1,5 @@
 def duplicate_nums(nums):
     count = {}
     for num in nums:
-        if num in count:
-            count[num] = False
-        else:
-            count[num] = True
-        return sorted(list(filter(lambda x: not count[x],count.keys())))
+        count[num] = num not in count
+    return sorted(list(filter(lambda x: not count[x],count.keys())))
